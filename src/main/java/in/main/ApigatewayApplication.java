fdf6ea0173829265af1693c567d6cd9727329823
adinathbiradar <adinathbiradar77@gmail.com>
2025-09-30 11:56:53 +0530

    Initial commit

---
+++ src/main/java/in/main/ApigatewayApplication.java
@@ -0,0 +1,14 @@
+package in.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+		System.out.println("apigateway");
+	}
+
+}
