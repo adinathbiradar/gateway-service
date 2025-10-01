e83b2485c12c49329bc2768401897d989bf5128f
adinathbiradar <adinathbiradar77@gmail.com>
2025-10-01 17:00:21 +0530

    controller added

---
+++ src/main/java/in/main/controller/GatewayTestController.java
@@ -0,0 +1,22 @@
+package in.main.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GatewayTestController {
+
+    @GetMapping("/test-config")
+    public Map<String, Object> testConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put("serverPort", 8082);
+        config.put("routes", List.of("hotels-service", "users-service", "bookings-service", "contacts-service"));
+        return config;
+    }
+}
+
