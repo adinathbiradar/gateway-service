124d6a13cf7b7eb2067656389c94335781095653
adinathbiradar <adinathbiradar77@gmail.com>
2025-10-04 10:16:28 +0530

    updated

--- src/main/java/in/main/controller/GatewayTestController.java
+++ src/main/java/in/main/controller/GatewayTestController.java
@@ -1,6 +1,7 @@
 package in.main.controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,10 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {
+	    "http://localhost:4200",
+	    "https://hotelbookingbyadinath.netlify.app"
+	})
 public class GatewayTestController {
 
     @GetMapping("/test-config")
